071e757cc6d1b02d3f576d3dd24335ea1a7162d5
netravler <pheintz@hotmail.com>
2017-04-28 09:32:08 -0500

    more
    more

---
+++ rebootMeListByDC.ps1
@@ -0,0 +1,90 @@
+﻿#
+# This script calls a custom cmdlt that will generate a report by DC and serverType
+#
+# ie (Get-NTserver -datacenter lks -serverType ASPWEB | select name | foreach { $_.Name } | sort name)
+#
+#	Author: Paul R Heintz ISG Release 
+#
+#	Revisions: 
+#
+#
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# working variables
+$reportDate = Get-Date -Format yyyyMMdd_HHmm
+
+$nl = "`r`n"
+
+# functions
+
+function rebootMeList ($serTypes)
+{
+	$DC = @("lks",
+			"bmo")
+			
+	# blowout to an array
+	$serverType = $serTypes -split " " 
+			
+	foreach ($dcLocation in $DC)
+	{
+		foreach ($SType in $serverType)
+		{
+			$outputListserverTypeByLocation = Get-NTserver -datacenter $dcLocation -serverType $SType | select name | foreach { $_.Name } | sort name 
+			
+			# ok take the array and send it to the txt file with headings containing location and serverType
+			for ($I = 0; $I -lt $outputListserverTypeByLocation.Length; $I++)
+			{
+				$outLine += $outputListserverTypeByLocation[$I] + " " 
+			}
+			
+			# add to $tbBoxReBoot
+			$tbBoxReBoot.AppendText($SType + " " + $outLine + $nl) > $null
+			
+			$outLine = ""
+		}
+	}
+}
+
+# form info here:
+
+$Form = New-Object system.Windows.Forms.Form
+$Form.Text = "Boot Servers List Generator"
+$Form.Size = New-Object System.Drawing.Size(600,500)
+
+# ListBoxReBoot
+$tbBoxReBoot = New-Object System.Windows.Forms.TextBox
+$tbBoxReBoot.Location = New-Object System.Drawing.Size(20,120)
+$tbBoxReBoot.size = New-Object System.Drawing.Size(300,300)
+$tbBoxReBoot.Multiline = $true
+$tbBoxReBoot.ScrollBars = "Vertical"
+
+$Form.Controls.Add($tbBoxReBoot)
+
+# execute button
+$b1 = New-Object System.Windows.Forms.Button
+$b1.Location = New-Object System.Drawing.Size(20,20)
+$b1.Size = New-Object System.Drawing.Size(75,53)
+$b1.Text = 'Generate List'
+$b1.Add_Click({ rebootMeList $tbEnv.Text })
+
+$Form.Controls.Add($b1)
+
+# environment list
+$tbEnv = New-Object System.Windows.Forms.TextBox 
+$tbEnv.Location = New-Object System.Drawing.Size(120,20) 
+$tbEnv.Size = New-Object System.Drawing.Size(260,20)
+ 
+$Form.Controls.Add($tbEnv) 
+
+# Label Release part
+$tbEnvlbl = New-Object System.Windows.Forms.Label
+$tbEnvlbl.Location = New-Object System.Drawing.Size(380,20) 
+$tbEnvlbl.Size = New-Object System.Drawing.Size(260,20)
+$tbEnvlbl.Text = "List seperated by a space"
+
+$Form.Controls.Add($tbEnvlbl) 
+
+$Form.ShowDialog()
+
+exit
---
+++ soYouWantToCreateAnEmail.ps1
@@ -0,0 +1,28 @@
+﻿
+$ol = New-Object -comObject Outlook.Application
+
+$environmentMigPro = ""
+
+$mail = $ol.CreateItem(0)
+
+$PrepSubject = "Defect #DEFECTDEFECT# -  #ENVENVEVN# for DotNet Build Please push part #PARTPART#"
+
+$mail.Subject = $PrepSubject -replace "#DEFECTDEFECT#", $tbRelease.Text -replace "#PARTPART#", $tbletterPart.Text -replace "#ENVENVENV#", $evMigPro
+
+$PrepBody = "Operations,
+
+Please execute the following (Code is in the Defect #DEFECTDEFECT#):
+
+1.	Run Part #PARTPART#_LX on LKSNTADM01, and run Part #PARTPART#_BMO on BMONTADM01
+2.	No need to manually run the Migration Robots
+
+This code will not be live until we verify the build comes up successfully.
+
+Thank you,"
+
+$mail.Body = $PrepBody -replace "#DEFECTDEFECT#", $tbRelease.Text -replace "#PARTPART#", $tbletterPart.Text
+
+$mail.save()
+
+$inspector = $mail.GetInspector
+$inspector.Display()
